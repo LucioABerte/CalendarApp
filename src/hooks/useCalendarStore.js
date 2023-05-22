8f11b0b8073fbf3fcbe3dbd1bf884c9544d1e909
Lucio Berté <lucioaberte@gmail.com>
2023-05-22 12:46:46 -0300

    actualizar el evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from '../store'
 import { calendarApi } from '../api'
 import { convertEventsToDateEvents } from '../helpers'
+import Swal from 'sweetalert2'
 
 export const useCalendarStore = () => {
     
@@ -16,15 +17,20 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async( calendarEvent ) => {
 
-
-        if( calendarEvent._id){
-            // Actualizando
-            dispatch(onUpdateEvent({...calendarEvent}))
-        }else {
-            //Creando
-            const {data} = await calendarApi.post("/events", calendarEvent)
-            
-            dispatch(onAddNewEvent( {...calendarEvent, id: data.evento.id, user }))
+        try {
+            if( calendarEvent.id){
+                // Actualizando
+                await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
+                dispatch(onUpdateEvent({...calendarEvent, user}))
+                return
+            }
+                //Creando
+                const {data} = await calendarApi.post("/events", calendarEvent)
+                
+                dispatch(onAddNewEvent( {...calendarEvent, id: data.evento.id, user } ) )
+        } catch (error) {
+            console.log(error);
+            Swal.fire("Error al guardar", error.response.data.msg, "error")
         }
     }
 
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -33,7 +33,7 @@ export const calendarSlice = createSlice({
       },
       onUpdateEvent: (state, {payload}) => {
         state.events = state.events.map( event => {
-          if(event._id === payload._id){
+          if(event.id === payload.id){
             return payload
           }
 
@@ -42,7 +42,7 @@ export const calendarSlice = createSlice({
       },
       onDeleteEvent: (state) => {
         if(state.activeEvent) { 
-        state.events = state.events.filter( event => event._id !== state.activeEvent._id)
+        state.events = state.events.filter( event => event.id !== state.activeEvent.id)
         state.activeEvent = null
         }
       },
